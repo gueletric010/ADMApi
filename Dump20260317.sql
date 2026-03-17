7f6aad83c8c0307e5b86cf3ca0f4f7b37806be5d
gueletric010 <gueletric@gmail.com>
2026-03-17 09:09:35 -0300

    v20260317

---
+++ Dump20260317.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE  IF NOT EXISTS `adm` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `adm`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: adm
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `lançamento`
+--
+
+DROP TABLE IF EXISTS `lançamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lançamento` (
+  `idLançamento` int unsigned NOT NULL AUTO_INCREMENT,
+  `idProduto` int NOT NULL,
+  `dtEmissao` date NOT NULL,
+  `vlNota` decimal(14,2) unsigned zerofill NOT NULL,
+  `nrAliquotaICMS` decimal(4,2) unsigned zerofill NOT NULL,
+  `nrAliquotaIPI` decimal(4,2) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`idLançamento`),
+  UNIQUE KEY `idLeitura_UNIQUE` (`idLançamento`),
+  KEY `fk_Leitura_Sensor_idx` (`idProduto`),
+  CONSTRAINT `fk_Leitura_Sensor` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lançamento`
+--
+
+LOCK TABLES `lançamento` WRITE;
+/*!40000 ALTER TABLE `lançamento` DISABLE KEYS */;
+INSERT INTO `lançamento` VALUES (1,1,'2026-03-17',000000000200.00,17.00,00.00);
+/*!40000 ALTER TABLE `lançamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto` (
+  `idProduto` int NOT NULL AUTO_INCREMENT,
+  `nmProduto` varchar(200) NOT NULL,
+  `nmUnidadeMedida` varchar(200) NOT NULL,
+  `nrNCM` int NOT NULL,
+  PRIMARY KEY (`idProduto`),
+  UNIQUE KEY `idSensor_UNIQUE` (`idProduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'Arla','Lt',9999999),(2,'Diesel','Lt',9999988),(3,'Parabrisa','UN',552233101);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-17  8:55:22
